39a8eb827ae928aa93efa57ef3e205f818272bfa
Pio <p990301@gmail.com>
2022-08-30 10:16:15 +0900

    csv convert

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import convert_csv
 
-convert_csv.generate_csv("C:\\Users\\HOME\\Desktop\\output.pcap", "Empty_Amp_267.csv", 'amplitude')
+convert_csv.generate_csv("C:\\Users\\HOME\\Desktop\\output.pcap", "input-22-08-03-03.csv", 'amplitude')
