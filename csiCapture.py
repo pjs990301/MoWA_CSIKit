ebc69dc94a053092b24129031a5f4da71017f5b7
Pio <p990301@gmail.com>
2022-09-03 21:25:26 +0900

    csi capture

---
+++ csiCapture.py
@@ -0,0 +1,10 @@
+import os
+
+# 자동으로 1000번 반복 (Empty)
+for i in range(1, 1000):
+    command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
+    os.system(command)
+
+# stand
+command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
+os.system(command)
