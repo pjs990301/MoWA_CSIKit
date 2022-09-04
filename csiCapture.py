f6c0836a3f3d2bcc5aba2414195fd270da6be3d0
Pio <p990301@gmail.com>
2022-09-05 00:31:05 +0900

    csi capture

--- csiCapture.py
+++ csiCapture.py
@@ -5,6 +5,6 @@ for i in range(1, 1000):
     command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
     os.system(command)
 
-# stand
-command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
-os.system(command)
+# # stand
+# command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
+# os.system(command)
--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import os
 
 import convert_csv
 
-path = "/Users/pyojisung/Library/CloudStorage/GoogleDrive-gachon.mowa@gmail.com/내 드라이브/data/pcap"
+# mac
+# path = "/Users/pyojisung/Library/CloudStorage/GoogleDrive-gachon.mowa@gmail.com/내 드라이브/data/pcap"
+
+# window
+path = "H:\\내 드라이브\\data\\pcap"
 os.chdir(path)
-for i in range(308, 1001):
-    file_name = "Empty_Ex_Home_" + str(i)
+for i in range(81, 122):
+    file_name = "Standing_Ex_Home_" + str(i)
     convert_csv.generate_csv(file_name + ".pcap",
-                             "/Users/pyojisung/Library/CloudStorage/GoogleDrive-gachon.mowa@gmail.com/내 드라이브/data/csv/"+file_name + ".csv", 'amplitude')
+                             # "/Users/pyojisung/Library/CloudStorage/GoogleDrive-gachon.mowa@gmail.com/내
+                             # 드라이브/data/csv/"+file_name + ".csv", 'amplitude')
+                             "H:\\내 드라이브\\data\\csv\\" + file_name + ".csv", 'amplitude')
