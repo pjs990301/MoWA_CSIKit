da19cb4b70d7a18f5a4d3d7b0bf24601e6dac792
Pio <p990301@gmail.com>
2022-09-07 00:44:11 +0900

    csi capture

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ import convert_csv
 # window
 path = "H:\\내 드라이브\\data\\pcap"
 os.chdir(path)
-for i in range(251, 400):
-    file_name = "Lying_Ex_Home_" + str(i)
+for i in range(162, 241):
+    # file_name = "Lying_Ex_Home_" + str(i)
+    file_name = "Standing_Ex_Home_" + str(i)
     convert_csv.generate_csv(file_name + ".pcap",
                              # "/Users/pyojisung/Library/CloudStorage/GoogleDrive-gachon.mowa@gmail.com/내
                              # 드라이브/data/csv/"+file_name + ".csv", 'amplitude')
