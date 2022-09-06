2b7dd0883fb1cc65ed997bc92e9a796a120f9a19
Pio <p990301@gmail.com>
2022-09-06 21:25:35 +0900

    csi capture

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import convert_csv
 # window
 path = "H:\\내 드라이브\\data\\pcap"
 os.chdir(path)
-for i in range(1, 251):
+for i in range(251, 400):
     file_name = "Lying_Ex_Home_" + str(i)
     convert_csv.generate_csv(file_name + ".pcap",
                              # "/Users/pyojisung/Library/CloudStorage/GoogleDrive-gachon.mowa@gmail.com/내
