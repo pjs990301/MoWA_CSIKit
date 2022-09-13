ffcff32663e60f702f449ae0dfbb67c911d69131
Pio <p990301@gmail.com>
2022-09-13 10:57:27 +0900

    자동 캡쳐 후 ftp 전송

--- csiCapture.py
+++ csiCapture.py
@@ -1,10 +1,36 @@
 import os
+import ftplib
+import schedule
+import time
+from datetime import datetime
 
-# 자동으로 1000번 반복 (Empty)
-for i in range(1, 1000):
-    command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
-    os.system(command)
 
-# # stand
-# command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Empty_Ex_Home_" + str(i) + ".pcap" + " -c 500"
-# os.system(command)
+def command():
+    now = datetime.now()
+    file_name = "input_" + now.strftime('%Y_%m_%d') + ".pcap"
+    cmd = "tcpdump -i wlan0 dst port 5500 -vv -w " + file_name + " -c 500"
+    os.system(cmd)
+    ftp(file_name)
+
+
+def ftp(file_name):
+    session = ftplib.FTP()
+    session.connect('blue-sun.kro.kr', 9002)  # 두 번째 인자는 port number
+    session.login("MoWA", "12345678")  # FTP 서버에 접속
+
+    uploadfile = open(file_name, mode='rb')  # 업로드할 파일 open
+
+    session.encoding = 'utf-8'
+    session.storbinary('MoWA ' + '/data/input/input.pcap', uploadfile)  # 파일 업로드
+
+    uploadfile.close()  # 파일 닫기
+
+    session.quit()  # 서버 나가기
+    print("전송 완료")
+
+
+schedule.every(1).minutes.do(command())
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
