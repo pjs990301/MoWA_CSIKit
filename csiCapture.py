51eb0e1c32c0538e1e3b7a6d7bbd1fbf28d4c6b8
Pio <p990301@gmail.com>
2022-09-27 10:26:22 +0900

    file ftp

--- csiCapture.py
+++ csiCapture.py
@@ -1,6 +1,6 @@
 import os
 
-for i in range(64, 101):
+for i in range(64, 102):
     command = "tcpdump -i wlan0 dst port 5500 -vv -w " + "Walking_Ex_Home_" + str(i) + ".pcap" + " -c 500"
     print(command)
     os.system(command)
