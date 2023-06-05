5fe786f3318c23c81406b12098baa8ce20cd1c2f
Pio <p990301@gmail.com>
2023-06-05 20:48:35 +0900

    Update csiCapture_live.py
    요청 방식 변경 
    FTP -> REST API로 변경

--- csiCapture_live.py
+++ csiCapture_live.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import sys
 sys.path.append("/usr/lib/python3/dist-packages/")
 
 import os
 import ftplib
-import schedule
+import schedule 
 import time
 from datetime import datetime
-
+import requests
 
 def command():
     now = datetime.now()
@@ -15,29 +17,24 @@ def command():
     os.system(cmd)
     return file_name
 
+def upload_file(file_name):
+    url = "http://3.37.161.170:8000/pi/CSI"  # 요청을 보낼 URL
 
-def ftp(file_name):
-    session = ftplib.FTP()
-    session.connect('blue-sun.kro.kr', 9002)
-    session.login("MoWA", "12345678")
+    with open(file_name, 'rb') as file:
+        files = {'file': file}
+        response = requests.post(url, files=files)
 
-    uploadfile = open(file_name, mode='rb')
-
-    session.encoding = 'utf-8'
-    session.storbinary('STOR '+'/data/input/pcap/' + file_name, uploadfile)
-    uploadfile.close()
-
-    session.quit()
-    print("complete")
+    if response.status_code == 200:
+        print('파일 업로드 성공')
+    else:
+        print('파일 업로드 실패')
 
 def job():
     file_name = command()
-    ftp(file_name)
+    upload_file(file_name)
 
-
-# schedule.every(1).minutes.do(job)
-schedule.every(30).seconds.do(job)
 while True:
     schedule.run_pending()
     time.sleep(1)
+    schedule.every(15).seconds.do(job)
 
